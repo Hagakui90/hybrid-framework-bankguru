8b0c219bd5f54d3924a77be25ac57ee3890f0bdd
Hana Pham <hagakui90@gmail.com>
2024-10-01 16:22:50 +0700

    update Base Page

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,65 +1,74 @@
 package commons;
 
-
+import java.util.List;
 import java.util.Set;
 
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
-	
+
 	/* Web Browser */
 	public void openPageUrl(WebDriver driver, String pageUrl) {
 		driver.get(pageUrl);
 	}
-	
+
 	public String getPageTitle(WebDriver driver) {
 		return driver.getTitle();
 	}
-	
+
 	public String getCurrentPageUrl(WebDriver driver) {
 		return driver.getCurrentUrl();
 	}
-	
+
 	public String getPageSourceCode(WebDriver driver) {
 		return driver.getPageSource();
 	}
-	
+
 	public void backToPage(WebDriver driver) {
 		driver.navigate().back();
 	}
-	
+
 	public void forwardToPage(WebDriver driver) {
 		driver.navigate().forward();
 	}
-	
+
 	public void refreshCurrentPage(WebDriver driver) {
 		driver.navigate().refresh();
 	}
-	
+
 	public Alert waitForAlertPresence(WebDriver driver) {
 		return new WebDriverWait(driver, 30).until(ExpectedConditions.alertIsPresent());
 	}
-	
+
 	public void acceptToAlert(WebDriver driver) {
 		waitForAlertPresence(driver).accept();
 	}
-	
+
+	public void cancelAlert(WebDriver driver) {
+		waitForAlertPresence(driver).dismiss();
+	}
+
 	public String getTextInAlert(WebDriver driver) {
 		return waitForAlertPresence(driver).getText();
 	}
-	
+
 	public void sendkeyToAlert(WebDriver driver, String keysToSend) {
 		waitForAlertPresence(driver).sendKeys(keysToSend);
 	}
 
-	public void switchToWindowByID(WebDriver driver, String parentID) {
+	public void switchToWindowByID(WebDriver driver, String pageId) {
 		Set<String> allWindowIDs = driver.getWindowHandles();
 
 		for (String id : allWindowIDs) {
-			if (!id.equals(parentID)) {
+			if (!id.equals(pageId)) {
 				driver.switchTo().window(id);
 				sleepInSecond(2);
 			}
@@ -98,6 +107,17 @@ public class BasePage {
 
 	}
 
+	public Set<Cookie> getAllCookies(WebDriver driver) {
+		return driver.manage().getCookies();
+	}
+
+	public void setCookies(WebDriver driver, Set<Cookie> cookies) {
+		for (Cookie cookie : cookies) {
+			driver.manage().addCookie(cookie);
+		}
+		sleepInSecond(3);
+	}
+
 	public void sleepInSecond(long timeInSecond) {
 		try {
 			Thread.sleep(timeInSecond * 1000);
@@ -105,4 +125,78 @@ public class BasePage {
 			e.printStackTrace();
 		}
 	}
+
+	/* Web Element */
+	public By getByXpath(String locator) {
+		return By.xpath(locator);
+	}
+
+	public WebElement getWebElement(WebDriver driver, String locator) {
+		return driver.findElement(getByXpath(locator));
+	}
+
+	public List<WebElement> getListWebElement(WebDriver driver, String locator) {
+		return driver.findElements(getByXpath(locator));
+	}
+
+	public void clickToElement(WebDriver driver, String locator) {
+		getWebElement(driver, locator).click();
+	}
+
+	public void sendkeyToElement(WebDriver driver, String locator, String valueToSend) {
+		getWebElement(driver, locator).clear();
+		getWebElement(driver, locator).sendKeys(valueToSend);
+	}
+
+	public void selectItemInDefaultDropdown(WebDriver driver, String locator, String itemValue) {
+		new Select(getWebElement(driver, locator)).selectByVisibleText(itemValue);
+	}
+
+	public String getFirstSelectedTextInDefaultDropdown(WebDriver driver, String locator) {
+		return new Select(getWebElement(driver, locator)).getFirstSelectedOption().getText();
+	}
+
+	public boolean isDefaulDropdownMultiple(WebDriver driver, String locator) {
+		return new Select(getWebElement(driver, locator)).isMultiple();
+	}
+
+	public void selectItemInCustomDropdown(WebDriver driver, String parentLocator, String childLocator, String textExpectedItem) {
+		getWebElement(driver, parentLocator).click();
+		sleepInSecond(1);
+
+		List<WebElement> allItems = new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfAllElementsLocatedBy(getByXpath(childLocator)));
+
+		for (WebElement item : allItems) {
+
+			String textActualItem = item.getText();
+			if (textActualItem.equals(textExpectedItem)) {
+				item.click();
+				break;
+
+			}
+		}
+	}
+
+	public String getElementText(WebDriver driver, String locator) {
+		return getWebElement(driver, locator).getText();
+	}
+
+	public String getElementAttribute(WebDriver driver, String locator, String attributeName) {
+		return getWebElement(driver, locator).getAttribute(attributeName);
+	}
+
+	public String getElementCssValue(WebDriver driver, String locator, String cssPropertyName) {
+		return getWebElement(driver, locator).getCssValue(cssPropertyName);
+	}
+
+	public String convertRGBAToHexaColor(WebDriver driver, String locator) {
+		return Color.fromString(getElementCssValue(driver, locator, "background-color")).asHex();
+	}
+	
+	public int getListElementSize(WebDriver driver, String locator) {
+		return getListWebElement(driver, locator).size();
+	}
+	
+	
+	
 }
